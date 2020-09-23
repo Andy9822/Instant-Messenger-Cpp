e56c539abafc1332ad68353b41dcb305e58d3d1c
gabriel-inf <gabriel.stepien@inf.ufrgs.br>
2020-09-23 00:21:14 -0300

    Refactor architecture and create some interfaces

---
+++ Instant-Messenger/include/client/user_interface.h
@@ -0,0 +1,14 @@
+#include <string>
+
+using namespace std;
+using std::string;
+
+namespace userinterface {
+
+
+class UserInteface {
+    public: 
+        UserInteface();
+
+};
+}  // namespace clientmessagemanager
---
+++ Instant-Messenger/include/util/message.h
@@ -0,0 +1,30 @@
+#include <string>
+#include <ctime>
+
+using namespace std;
+using std::string;
+
+namespace message {
+
+#define BUFFER_SIZE 10
+
+class Message {
+
+    private:
+        string text;
+        string user;
+        string group;
+        time_t time;
+    
+    public:
+        string getText();
+        string setText(string text);
+        string getUser();
+        string setUser(string user);
+        string getGroup();
+        string setGroup(string group);
+        string getTime();
+        string setTime(time_t time);
+
+};
+}  // namespace message
