ca8ad5738afa1afb4c55971b46ee21232da0969e
gabriel <gabriel.stepien@inf.ufrgs.br>
2020-09-27 01:52:13 -0300

    ident user class to match google code style.

--- Instant-Messenger/src/util/user.cpp
+++ Instant-Messenger/src/util/user.cpp
@@ -4,44 +4,44 @@
 
 namespace user{
 
-    User::User() {
-        for (int i = 0; i < NUMBER_OF_SIMULTANEOUS_CONNECTIONS; i++) {
-            this->sockets[i] = 0;
-        }
+User::User() {
+    for (int i = 0; i < NUMBER_OF_SIMULTANEOUS_CONNECTIONS; i++) {
+        this->sockets[i] = 0;
     }
+}
 
-    User::User(string username) {
-        this->username = username;
-    }
+User::User(string username) {
+    this->username = username;
+}
 
-    string User::getUsername() {
-        return this->username;
-    }
+string User::getUsername() {
+    return this->username;
+}
 
-    void User::getActiveSockets(int* activeSocketsResult) {
-        for (int i = 0; i < NUMBER_OF_SIMULTANEOUS_CONNECTIONS ; i++) {
-            if (this->sockets[i] != 0) {
-                activeSocketsResult[i] = this->sockets[i];
-            }
-        }
-    }
-
-    void User::registerSession(int socket) {
-        for (int i = 0; i < NUMBER_OF_SIMULTANEOUS_CONNECTIONS; i++) {
-            if (this->sockets[i] == 0){
-                this->sockets[i] = socket;
-                return;
-            }
-        }
-        throw USER_SESSIONS_LIMIT_REACHED;
-    }
-
-    void User::releaseSession(int socket) {
-        for (int i = 0; i < NUMBER_OF_SIMULTANEOUS_CONNECTIONS; i++) {
-            if (this->sockets[i] == socket){
-                this->sockets[i] = 0;
-                return;
-            }
+void User::getActiveSockets(int* activeSocketsResult) {
+    for (int i = 0; i < NUMBER_OF_SIMULTANEOUS_CONNECTIONS ; i++) {
+        if (this->sockets[i] != 0) {
+            activeSocketsResult[i] = this->sockets[i];
         }
     }
 }
+
+void User::registerSession(int socket) {
+    for (int i = 0; i < NUMBER_OF_SIMULTANEOUS_CONNECTIONS; i++) {
+        if (this->sockets[i] == 0){
+            this->sockets[i] = socket;
+            return;
+        }
+    }
+    throw USER_SESSIONS_LIMIT_REACHED;
+}
+
+void User::releaseSession(int socket) {
+    for (int i = 0; i < NUMBER_OF_SIMULTANEOUS_CONNECTIONS; i++) {
+        if (this->sockets[i] == socket){
+            this->sockets[i] = 0;
+            return;
+        }
+    }
+}
+} // namespace user;
