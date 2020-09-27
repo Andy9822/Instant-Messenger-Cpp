3cc9e7b82b8fd5804ecdbadf6c2c9d001fe80f01
gabriel <gabriel.stepien@inf.ufrgs.br>
2020-09-27 17:49:23 -0300

    Adds function to read messages from group

--- Instant-Messenger/include/server/server_group_manager.h
+++ Instant-Messenger/include/server/server_group_manager.h
@@ -1,9 +1,11 @@
 #include <string>
 #include "include/util/message.hpp"
+#include "include/util/user.hpp"
 
 using namespace std;
 using std::string;
 using namespace message;
+using namespace user;
 namespace servermessage {
 
 class ServerMessageManager {
@@ -15,7 +17,7 @@ class ServerMessageManager {
         * This method is responsible for sending the messages to the
         * communication layer
         */
-        void sendMessage(Message message, Users users);
+        void sendMessage(Message message, User* users);
  
 };
 }  //namespace clientmessagemanager
