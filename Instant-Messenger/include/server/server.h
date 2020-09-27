b923a16fa62f93fa00757f85a85f02c2820fa374
Andy Ruiz Garramones <andyfutbolclip@yahoo.es>
2020-09-27 22:45:55 +0200

    wip

--- Instant-Messenger/include/server/server.h
+++ Instant-Messenger/include/server/server.h
@@ -8,6 +8,8 @@
 #include <netinet/in.h>
 #include <arpa/inet.h>
 #include <pthread.h>
+#include <vector>
+#include <algorithm>
 
 #define MAXBACKLOG SOMAXCONN
 
@@ -17,7 +19,9 @@ class Server
 	private:
 		int socket_fd;
 		struct sockaddr_in serv_addr;
-
+		//TODO maybe remove static and evaluate how to share vector between threads
+	public:
+		static std::vector <int> openSockets;
 	public:
 		Server();
 		void setPort(int port);
@@ -25,6 +29,7 @@ class Server
 		void printPortNumber();
 		int ConnectToClient(pthread_t *tid);
 		static void* clientCommunication(void *newsocket);
+		static void closeClientConnection(int socket_fd);
 		void closeConnections();
 		void closeSocket();
 		void closeServer();
