b590c3c31f2127b4326783e4fd2417b97edf1b29
gschaves <guilherme.chaves@inf.ufrgs.br>
2020-10-01 00:27:40 -0300

    Create group manager and limit user sessions in one group
    checking more than two entrances
    
    implements semaphore with mutex
    
    Removes unecessary import
    
    adds back post
    
    removes prints
    
    removes unecessary thread for the user registration
    
    Removes unecessary static
    
    go
    
    some changes

--- Instant-Messenger/src/server/server.cpp
+++ Instant-Messenger/src/server/server.cpp
@@ -5,6 +5,8 @@ sem_t Server::semaphore;
 
 Server::Server()
 {
+	groupManager = new ServerGroupManager();
+
 	// Configure server address properties
 	serv_addr.sin_family = AF_INET;
 	serv_addr.sin_addr.s_addr = INADDR_ANY;
@@ -14,12 +16,16 @@ Server::Server()
 	socket_fd = 0;
 }
 
+
+
 void Server::closeClientConnection(int socket_fd)
 {
 	std::cout << "Closing socket: " << socket_fd << std::endl;
 	openSockets.erase(std::remove(openSockets.begin(), openSockets.end(), socket_fd), openSockets.end());
 	close(socket_fd);
 }
+
+
 
 void Server::closeConnections()
 {
@@ -29,6 +35,8 @@ void Server::closeConnections()
 	cout << "Number of client connections: " << openSockets.size() << endl;
 }
 
+
+
 void Server::closeSocket()
 {
 	std::cout << "server_socket_fd: " << socket_fd << std::endl;
@@ -36,16 +44,22 @@ void Server::closeSocket()
 	cout << "Closing server socket..." << endl;
 }
 
+
+
 void Server::closeServer() 
 {
 	closeConnections();
 	closeSocket();
 }
 
+
+
 void Server::setPort(int port) 
 {
 	serv_addr.sin_port = htons(port);
 }
+
+
 
 void Server::prepareConnection()
 {
@@ -93,8 +107,54 @@ void Server::printPortNumber()
 
 
 
+int Server::registerUser(int socket, char* username, char* group)
+{
+	return groupManager->registerUserToServer(socket, username, group);
+}
+
+
+
+int Server::registerUserToServer(void* args)
+{
+	char username[USERNAME_MAX_SIZE] = {0};
+	char group[GROUP_MAX_SIZE] = {0};
+	
+	std::pair <int*,Server*>* args_pair = (std::pair <int*,Server*> *) args;
+	int client_socketfd = *(int *) args_pair->first;
+	Server* _this = (Server *)  args_pair->second;
+	
+	bool connectedClient = true;
+	Packet *pack = new Packet();
+
+	// getting client info
+	Packet* receivedPacket = _this->readPacket(client_socketfd, &connectedClient);
+
+	strncpy(username, receivedPacket->username, USERNAME_MAX_SIZE - 1);
+    strncpy(group, receivedPacket->group, GROUP_MAX_SIZE - 1);
+
+    // checking if there are already 2 users with the same name registered
+	if(_this->registerUser(client_socketfd, username, group) < 0)
+	{
+		pack->clientSocket = -1;
+		_this->sendPacket(client_socketfd, pack);
+		delete pack;
+		return -1;
+
+	}
+
+	pack->clientSocket = 0;
+	_this->sendPacket(client_socketfd, pack);
+	delete pack;
+
+	return 0;
+}
+
+
+
 int Server::handleClientConnection(pthread_t *tid)
 {
+	int status;
+
 	// Allocate memory space to store value in heap and be able to use it after this function ends
 	int* newsockfd = (int *) calloc(1,sizeof(int));
 	struct sockaddr_in cli_addr;
@@ -104,6 +164,22 @@ int Server::handleClientConnection(pthread_t *tid)
 	{
 		cout << "ERROR on accept\n" << endl;
 		return -1;
+	}
+
+	// Registering user to server
+	std::pair <int*,Server*>* user = (std::pair <int*,Server*>*) calloc(1,sizeof(std::pair <int*,Server*>));
+	user->first = newsockfd;
+	user->second = this;
+
+	status = this->registerUserToServer(user);
+	
+	free(user);
+
+	// if there are already two users with the same name -> close and ignore connection
+	if(status < 0)
+	{
+		close(*newsockfd);
+		return 0;
 	}
 
 	std::cout << "client connected  with socket: " << *newsockfd << std::endl;
@@ -126,6 +202,8 @@ int Server::handleClientConnection(pthread_t *tid)
 
 	return 0;
 }
+
+
 
 void* Server::clientCommunication(void *args)
 {
@@ -161,7 +239,7 @@ void* Server::clientCommunication(void *args)
 		cout << "Room: " << receivedPacket->group  << endl;
 		cout << "[Message]: " << receivedPacket->message  << endl;
 
-		Packet* sendingPacket = new Packet(receivedPacket->group, "Recebi sua mensagem!");
+		Packet* sendingPacket = new Packet(receivedPacket->username, receivedPacket->group, (char*)"Recebi sua mensagem!");
 		_this->sendPacket(client_socketfd, sendingPacket);
 	}
 
@@ -171,6 +249,8 @@ void* Server::clientCommunication(void *args)
 
 	return 0;
 }
+
+
 
 void Server::closeClientCommunication(int client_socket) 
 {
@@ -191,6 +271,8 @@ void Server::closeClientCommunication(int client_socket)
 		std::cout << "!!! Fatal error closing socket!!!!" << std::endl;
 	 }
 }
+
+
 
 // TODO may be move functions below to a binary semaphore class and just extend that class
 void Server::init_semaphore() {
