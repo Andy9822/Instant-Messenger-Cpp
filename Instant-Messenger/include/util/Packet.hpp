506d30b7c3d9b1a0282ff4fc1e3fa90cb020375d
Andy Ruiz Garramones <andyfutbolclip@yahoo.es>
2020-10-03 23:31:41 +0200

    divide client communication in sender and receiver thread

--- Instant-Messenger/include/util/Packet.hpp
+++ Instant-Messenger/include/util/Packet.hpp
@@ -3,6 +3,7 @@
 
 #include <string.h> 
 #include "definitions.hpp"
+#include <ctime>
 
 using namespace std;
 
@@ -13,14 +14,16 @@ struct Packet
     char group[GROUP_MAX_SIZE];
     char message[MESSAGE_MAX_SIZE];
     int clientSocket;
+    time_t timestamp;
 
     Packet() {}
 
-    Packet(char* username, char* group, char* message) {
+    Packet(char* username, char* group, char* message, time_t timestamp) {
       this->status = 1440;
       strncpy(this->username, username, USERNAME_MAX_SIZE - 1);
       strncpy(this->group, group, GROUP_MAX_SIZE - 1);
       strncpy(this->message, message, MESSAGE_MAX_SIZE - 1);
+      this->timestamp = timestamp;
     }
 };
 
