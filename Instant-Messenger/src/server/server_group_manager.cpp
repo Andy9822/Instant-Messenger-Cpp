aa304ec9c61c4bb6c9302839b0e4f60821d2d0f8
gabriel <gabriel.stepien@inf.ufrgs.br>
2020-10-03 18:58:27 -0300

    Fixes user instances

--- Instant-Messenger/src/server/server_group_manager.cpp
+++ Instant-Messenger/src/server/server_group_manager.cpp
@@ -1,9 +1,9 @@
 #include "../../include/server/server_group_manager.hpp"
 #include "../../include/util/user.hpp"
 
+using namespace std;
 
-
-ServerGroupManager::ServerGroupManager()
+ServerGroupManager::ServerGroupManager() : semaphore(1)
 {}
 
 
@@ -11,9 +11,11 @@ ServerGroupManager::ServerGroupManager()
 
 int ServerGroupManager::registerUserToGroup(int socket, string username, string group)
 {
+	this->semaphore.wait();
 	bool userAlreadyExists = false;
 	User *newUser;
 	list<User*>::iterator user;
+	int result = 0;
 	
 	for (user = list_users.begin(); user != list_users.end(); ++user)
 	{
@@ -21,7 +23,8 @@ int ServerGroupManager::registerUserToGroup(int socket, string username, string 
     	{
     		if((*user)->registerSession(socket) < 0)
 			{
-				return -1;
+				result = -1;
+				break;
 			}
     			
     		addUserToGroup(*user, group);
@@ -31,24 +34,44 @@ int ServerGroupManager::registerUserToGroup(int socket, string username, string 
     	}
 	}
 
+	this->semaphore.post();
 
-	if(userAlreadyExists == false)
+	if(!userAlreadyExists)
 	{
+		this->semaphore.wait();
 		newUser = new User(username);
 		
 		if(newUser->registerSession(socket) < 0)
-			return -1;
+			result = -1;
 		
 		addUserToGroup(newUser, group);
 		list_users.push_back(newUser);
-	}
-
-	return 0;
+		this->semaphore.post();
+	}	
+	
+	return result;
 }
-
-
 
 void ServerGroupManager::addUserToGroup(User *user, string userGroup)
 {
-	 group.insert(std::pair<string,User*>(userGroup,user));  
+	cout << "DEBUG: addUserToGroup" << endl;
+	std::vector<User*> groupUsers = getUsersByGroup(userGroup);
+	for (auto userItr = groupUsers.begin(); userItr != groupUsers.end(); userItr++) {
+		if ((*userItr)->getUsername() == user->getUsername()){
+			return;	
+		}
+	}
+	group.insert(std::pair<string,User*>(userGroup,user));  
+}
+
+std::vector<User*> ServerGroupManager::getUsersByGroup(string groupName) {
+	std::vector<User*> users;
+	cout << "DEBUG: getUsersByGroup" << endl;
+	for (auto itr = group.begin(); itr != group.end(); itr++) {
+		if (itr -> first == groupName) {
+			users.push_back(itr->second);
+			cout << itr -> first << "  " << itr -> second->getUsername() << endl;	// TODO: REMOVE
+		}        		
+	}
+	return users;
 }
