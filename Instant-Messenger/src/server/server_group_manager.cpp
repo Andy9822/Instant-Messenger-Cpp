9e195b090a3a7987e06e0a404cffcc74bbcf08e8
Cassiano Stradolini <cassiano.stradolini@sap.com>
2020-10-06 02:45:37 -0300

    adding release session for clients when connection is closed

--- Instant-Messenger/src/server/server_group_manager.cpp
+++ Instant-Messenger/src/server/server_group_manager.cpp
@@ -14,7 +14,7 @@ namespace servergroupmanager {
         User *newUser;
         list<User *>::iterator user;
         int result = 0;
-        std::pair<int, std::string> newSocket (socket, group);
+        std::pair<int, std::string> newSocket(socket, group);
 
         for (user = list_users.begin(); user != list_users.end(); ++user) {
             if ((*user)->getUsername() == username) {
@@ -58,7 +58,7 @@ namespace servergroupmanager {
 
     std::list<User *> ServerGroupManager::getUsersByGroup(string groupName) {
         std::list<User *> users;
-        for (auto const& group : groups) {
+        for (auto const &group : groups) {
             if (group.first == groupName)
                 users.push_back(group.second);
         }
@@ -67,9 +67,81 @@ namespace servergroupmanager {
 
     void ServerGroupManager::processReceivedPacket(Packet *packet) {
         Message receivedMessage = Message(packet->message, packet->username, packet->group, std::time(0));
-        std::list<User*> users = getUsersByGroup(receivedMessage.getGroup());
+        std::list<User *> users = getUsersByGroup(receivedMessage.getGroup());
 
         fileSystemManager->appendGroupMessageToHistory(receivedMessage);
         messageManager->broadcastMessageToUsers(receivedMessage, users);
+    }
+
+    void ServerGroupManager::disconnectUser(int socketId) {
+        this->semaphore.wait();
+        User *user = getUserBySocketId(socketId);
+        disconnectSocket(user, socketId);
+
+        if (user->getActiveSockets()->size() == 0) {
+            removeUserFromListOfUsers(user);
+        }
+        this->semaphore.post();
+
+        printListOfUsers(); //debug purposes
+        printListOfGroups(); //debug purposes
+    }
+
+    void ServerGroupManager::disconnectSocket(User *user, int socketId) {
+        string groupBeingDisconnected = user->getActiveSockets()->find(socketId)->second;
+        bool hasSimultaneousConnectionToRemovedGroup = false;
+
+        user->getActiveSockets()->erase(socketId);
+
+        for(auto const& socket : *user->getActiveSockets()){
+            if(socket.second == groupBeingDisconnected){ // check the removed socket group with rest of user socket list
+                hasSimultaneousConnectionToRemovedGroup = true;
+            }
+        }
+
+        if(!hasSimultaneousConnectionToRemovedGroup){
+            multimap<string, User *>::iterator userGroupEntryToBeRemoved;
+            for (auto itr = groups.begin(); itr != groups.end(); itr++) {
+                // this is responsible for fetching the correct entry o groups map
+                // so it can be removed as there are no more connections from this user to the specified group
+                if ((itr->first == groupBeingDisconnected && itr->second->getUsername() == user->getUsername())) {
+                    userGroupEntryToBeRemoved = itr;
+                }
+            }
+
+            groups.erase(userGroupEntryToBeRemoved);
+        }
+    }
+
+    User *ServerGroupManager::getUserBySocketId(int socketId) {
+        User *user = NULL;
+
+        for (auto const &userItr : this->list_users) {
+            for (auto const &socket : *userItr->getActiveSockets()) {
+                if (socket.first == socketId) {
+                    user = userItr;
+                }
+            }
+        }
+
+        return user;
+    }
+
+    void ServerGroupManager::removeUserFromListOfUsers(User *user) {
+        list_users.remove(user);
+    }
+
+    void ServerGroupManager::printListOfUsers() {
+        for (auto const &user: list_users) {
+            cout << "\nUser: " << user->getUsername() << " with sockets: " << endl;
+            user->printSockets();
+        }
+    }
+
+    void ServerGroupManager::printListOfGroups() {
+        cout << endl ;
+        for (auto const &group: groups) {
+            cout << "Group: " << group.first << " with User: " << group.second->getUsername() << endl;
+        }
     }
 }
--- Instant-Messenger/src/util/user.cpp
+++ Instant-Messenger/src/util/user.cpp
@@ -2,59 +2,41 @@
 #include <sstream>
 #include <iostream>
 
-namespace user{
-
-
-User::User(string username) : semaphore(1) {
-    this->initSessionList();
-    this->username = username;
-}
-
-void User::initSessionList() {
-    this->semaphore.wait();
-    this->sockets = new std::map<int, string>();
-    this->semaphore.post();
-}
-
-
-string User::getUsername() {
-    return this->username;
-}
-
-std::map<int, std::string> * User::getActiveSockets() {
-    this->semaphore.wait();
-
-    auto *activeSocketsResult = new map<int, string>();
-    for (auto & socket : *sockets) {
-        activeSocketsResult->insert(std::make_pair(socket.first, socket.second));
+namespace user {
+    User::User(string username) : semaphore(1) {
+        this->initSessionList();
+        this->username = username;
     }
 
-    this->semaphore.post();
-
-    return activeSocketsResult;
-}
-
-int User::registerSession(pair<const int, basic_string<char>> socket) {
-    this->semaphore.wait();
-    if(this->sockets->size() < 2){
-        this->sockets->insert(socket);
+    void User::initSessionList() {
+        this->semaphore.wait();
+        this->sockets = new std::map<int, string>();
         this->semaphore.post();
-        return 0;
     }
-    this->semaphore.post();
-    return  -1;
-}
 
-/*void User::releaseSession(int socket) {
-    this->semaphore.wait();
-    for (int i = 0; i < NUMBER_OF_SIMULTANEOUS_CONNECTIONS; i++) {
-        if (this->sockets[i] == socket){
-            this->sockets[i] = 0;
+
+    string User::getUsername() {
+        return this->username;
+    }
+
+    std::map<int, std::string> *User::getActiveSockets() {
+        return sockets;
+    }
+
+    int User::registerSession(pair<const int, basic_string<char>> socket) {
+        this->semaphore.wait();
+        if (this->sockets->size() < 2) {
+            this->sockets->insert(socket);
             this->semaphore.post();
-            return;
+            return 0;
+        }
+        this->semaphore.post();
+        return -1;
+    }
+
+    void User::printSockets() {
+        for (auto const &socket : *sockets) {
+            cout << "printsockt::Socket: " << socket.first << " from group: " << socket.second << endl;
         }
     }
-    this->semaphore.post();
-}*/
-
 } // namespace user;
