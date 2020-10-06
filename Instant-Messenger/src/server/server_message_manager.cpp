dbe5eb7cff332c1e08f4cfd4104e06acf1d2faab
Cassiano Stradolini <cassiano.stradolini@sap.com>
2020-10-05 21:14:39 -0300

    removing comments

--- Instant-Messenger/src/server/server_message_manager.cpp
+++ Instant-Messenger/src/server/server_message_manager.cpp
@@ -9,16 +9,16 @@ namespace servermessagemanager {
         // as we don't have information between groups and sockets at this moment
 
         Packet* sendingPacket = new Packet((char*)message.getUser().c_str(), (char*)message.getGroup().c_str(), (char*)message.getText().c_str());
-        cout << "Packet: " << sendingPacket ->username << "  " << sendingPacket -> group << "  " << sendingPacket -> message << " TO--> \n";
+        //cout << "Packet: " << sendingPacket ->username << "  " << sendingPacket -> group << "  " << sendingPacket -> message << " TO--> \n";
 
         for (auto userItr = users.begin(); userItr != users.end(); userItr++) {
-            cout << "user: " << (*userItr)->getUsername() << "  WITH-->-\n";
+            //cout << "user: " << (*userItr)->getUsername() << "  WITH-->-\n";
             int* activeSockets = new int[NUMBER_OF_SIMULTANEOUS_CONNECTIONS];
             (*userItr)->getActiveSockets(activeSockets);
-            cout << "socket: " << activeSockets[0] << "  \n";
-            cout << "socket: " << activeSockets[1] << "  \n";
+            //cout << "socket: " << activeSockets[0] << "  \n";
+            //cout << "socket: " << activeSockets[1] << "  \n";
             for (int i = 0; i < NUMBER_OF_SIMULTANEOUS_CONNECTIONS ; i++) {
-                if (activeSockets[i] != 0 && (*userItr)->getUsername() != message.getUser()) {
+                if (activeSockets[i] != 0) {
                     sendPacket(activeSockets[i], sendingPacket);
                 }
             }
