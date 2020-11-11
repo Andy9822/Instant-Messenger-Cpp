9720e59679bc61cac1f2f3ee310f8b493f03db44
Cassiano Stradolini <cassiano.stradolini@sap.com>
2020-11-11 00:51:29 -0300

    fixing segmentation fault

--- Instant-Messenger/src/server/server.cpp
+++ Instant-Messenger/src/server/server.cpp
@@ -292,17 +292,14 @@ namespace server {
     }
 
     void *Server::acceptRMConnection(void *param) {
-        int *newsockfd = (int *) calloc(1, sizeof(int *));
-        socklen_t clilen = sizeof(struct sockaddr_in);
-
-        // reference to this class
-        Server *_this;
-        _this = (Server *) param;
-
-        cout << "Esperando conexão pelo socket " << _this->rm_listening_socket_fd << " de porta "
-             << ntohs(_this->rm_listening_serv_addr.sin_port) << endl;
-
         while(1) {
+            int *newsockfd = (int *) calloc(1, sizeof(int *));
+            socklen_t clilen = sizeof(struct sockaddr_in);
+
+            // reference to this class
+            Server *_this;
+            _this = (Server *) param;
+
             if ((*newsockfd = accept(_this->rm_listening_socket_fd, (struct sockaddr *) &_this->rm_listening_serv_addr,
                                      &clilen)) == -1) {
                 cout << "ERROR on accept\n" << endl;
